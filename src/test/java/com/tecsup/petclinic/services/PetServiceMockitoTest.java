0bcf8ad4d7efa4f3990cc2cdfcedc5acb562d4b6
Jaime <jgomezm@tecsup.edu.pe>
2025-10-20 16:50:56 -0500

    update

--- src/test/java/com/tecsup/petclinic/services/PetServiceMockitoTest.java
+++ src/test/java/com/tecsup/petclinic/services/PetServiceMockitoTest.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.bean.override.mockito.MockitoBean; // New import
 
 @Slf4j
 //@ExtendWith(MockitoExtension.class)
