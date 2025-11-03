c23e01074b91ee0e8217694ad97248bb61d96fa0
Mayela3018 <mayela.ticona@tecsup.edu.pe>
2025-11-03 17:24:37 -0500

    Cambios en la carpeta entities - Mayela T

--- src/main/java/com/tecsup/petclinic/entities/VetSpecialty.java
+++ src/main/java/com/tecsup/petclinic/entities/VetSpecialty.java
@@ -1,2 +1,44 @@
 package com.tecsup.petclinic.entities;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "vet_specialties")
+@IdClass(VetSpecialtyId.class)
+public class VetSpecialty {
+
+    @Id
+    private Integer vetId;
+
+    @Id
+    private Integer specialtyId;
+
+    private LocalDate certificationDate;
+    private int yearsExperience;
+    private boolean isPrimary;
+
+    public VetSpecialty() {}
+
+    public VetSpecialty(Integer vetId, Integer specialtyId, LocalDate certificationDate,
+                        int yearsExperience, boolean isPrimary) {
+        this.vetId = vetId;
+        this.specialtyId = specialtyId;
+        this.certificationDate = certificationDate;
+        this.yearsExperience = yearsExperience;
+        this.isPrimary = isPrimary;
+    }
+
+    public Integer getVetId() { return vetId; }
+    public Integer getSpecialtyId() { return specialtyId; }
+    public LocalDate getCertificationDate() { return certificationDate; }
+    public int getYearsExperience() { return yearsExperience; }
+    public boolean isPrimary() { return isPrimary; }
+
+    public void setVetId(Integer vetId) { this.vetId = vetId; }
+    public void setSpecialtyId(Integer specialtyId) { this.specialtyId = specialtyId; }
+    public void setCertificationDate(LocalDate certificationDate) { this.certificationDate = certificationDate; }
+    public void setYearsExperience(int yearsExperience) { this.yearsExperience = yearsExperience; }
+    public void setPrimary(boolean primary) { isPrimary = primary; }
+}
+
--- src/main/java/com/tecsup/petclinic/entities/VetSpecialtyId.java
+++ src/main/java/com/tecsup/petclinic/entities/VetSpecialtyId.java
@@ -1,4 +1,31 @@
 package com.tecsup.petclinic.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+public class VetSpecialtyId implements Serializable {
+    private Integer vetId;
+    private Integer specialtyId;
+
+    public VetSpecialtyId() {}
+    public VetSpecialtyId(Integer vetId, Integer specialtyId) {
+        this.vetId = vetId;
+        this.specialtyId = specialtyId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VetSpecialtyId)) return false;
+        VetSpecialtyId that = (VetSpecialtyId) o;
+        return Objects.equals(vetId, that.vetId) &&
+                Objects.equals(specialtyId, that.specialtyId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vetId, specialtyId);
+    }
+}
 
 
