0d812c7ccff1f6a59525cdf82efde96be47d3320
madelein-milagros <ramoschamorromilagros@gmail.com>
2025-11-03 17:49:49 -0500

    Milagros

--- src/main/java/com/tecsup/petclinic/services/VetSpecialtyService.java
+++ src/main/java/com/tecsup/petclinic/services/VetSpecialtyService.java
@@ -1,3 +1,33 @@
 package com.tecsup.petclinic.services;
 
+import com.tecsup.petclinic.entities.VetSpecialty;
+import com.tecsup.petclinic.entities.VetSpecialtyId;
+import com.tecsup.petclinic.repositories.VetSpecialtyRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
+public class VetSpecialtyService {
+
+    private final VetSpecialtyRepository repository;
+
+    public VetSpecialtyService(VetSpecialtyRepository repository) {
+        this.repository = repository;
+    }
+
+    public VetSpecialty asignarEspecialidad(VetSpecialty vs) {
+        return repository.save(vs);
+    }
+
+    public List<VetSpecialty> buscarPorVeterinario(Integer vetId) {
+        return repository.findByVetId(vetId);
+    }
+
+    public List<VetSpecialty> buscarPorEspecialidad(Integer specialtyId) {
+        return repository.findBySpecialtyId(specialtyId);
+    }
+
+    public void eliminarRelacion(VetSpecialty vs) {
+        repository.deleteById(new VetSpecialtyId(vs.getVetId(), vs.getSpecialtyId()));
+    }
+}
